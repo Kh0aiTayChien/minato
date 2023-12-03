c6561e692022a66021458c589b2fe627613c51c9
Vinh <getback1110@gmail.com>
2023-12-03 20:28:17 +0700

    fix header mb

--- resources/views/pages/location/sec3.blade.php
+++ resources/views/pages/location/sec3.blade.php
@@ -1,5 +1,5 @@
 <div class="sec3-location-pc d-none d-sm-block">
-    <p class="header1-sec3-location-pc">điểm kết nối muôn nơi</p>
+    <p class="header2-sec3-location-pc">điểm kết nối muôn nơi</p>
     <p class="header1-sec3-location-pc">Bên sông Lạch Tray</p>
     <img src="{{asset('images/location/sec3/mao-16 1.png')}}">
 </div>
