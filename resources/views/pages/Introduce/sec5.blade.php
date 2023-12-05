7cfd32f38b6a0d7fd5a489df678e0cf6e5eb6a7c
Vinh <getback1110@gmail.com>
2023-12-06 01:31:18 +0700

    fix giao dien sec2-HP + sec5-intro

--- resources/views/pages/Introduce/sec5.blade.php
+++ resources/views/pages/Introduce/sec5.blade.php
@@ -1,9 +1,9 @@
 <div class="sec5-intro d-none d-sm-block">
-    <div class="row-test content-sec5-intro">
-        <div class="col-md-4" style="padding-left: 4%">
-        <p class="header1-sec5-intro icielbegum space-51">ĐẲNG CẤP <br>VƯỢT TRỘI</p>
-        <p class="header2-sec5-intro icielbegum space-51">Từ chất lượng <br> Nhật Bản</p>
-        <p class="text-sec5-intro space-15 cabin-medium">The Minato Residence là dự án tiên phong tại Hải Phòng
+    <p class="header1-sec5-intro icielbegum space-51">ĐẲNG CẤP VƯỢT TRỘI</p>
+    <p class="header2-sec5-intro icielbegum space-51">Từ chất lượng Nhật Bản</p>
+    <div class="row-test-1 content-sec5-intro">
+        <div class="left-test-1" style="padding-left: 4%; padding-top: 4%">
+        <p class="text-content1-sec2-hp space-15 text-sec5-intro cabin-medium pb-4">The Minato Residence là dự án tiên phong tại Hải Phòng
             hội tụ đủ 100% yếu tố Nhật
             Bản, từ thiết kế, thi công đến giám sát, vận hành. Với chuỗi căn hộ cao cấp của mình, The Minato Residence
             mang đến cho khách hàng cơ hội trải nghiệm những tác phẩm kiến trúc tinh tế và sang trọng nhất, được sáng
@@ -11,7 +11,7 @@
             nghiệp, lựa chọn theo tiêu chuẩn quốc tế, phù hợp với kiến trúc cao tầng và đảm bảo an toàn cho các cư
             dân.</p>
         </div>
-        <img src="{{asset('images/introduce/sec5/Mask group (6).png')}}" class="col-md-8 img-sec5-intro">
+        <img src="{{asset('images/introduce/sec5/Mask group (6).png')}}" class="img-sec2-hp-1 right-test-1">
     </div>
 </div>
 
--- resources/views/pages/home-page/sec2.blade.php
+++ resources/views/pages/home-page/sec2.blade.php
@@ -1,44 +1,46 @@
 <div class="sec2-HP-pc d-none d-sm-block">
-
-    <div class="row-test">
-        <div class="col-md-4" style="padding-left: 4%">
-            <p class="header-first-line-hp co-header-css mulish-extrabold space-21">The Minato Residence</p>
-            <p class="header-second-line-hp co-header-css icielbegum space-51">TINH HOA NHẬT BẢN</p>
-            <p class="header-second-line-hp co-header-css icielbegum space-51">An yên giữa lòng phố Cảng</p><br>
-            <p class="first-text-hp space-15 text-hp cabin-medium pb-4">Tiên phong mang đến một làn gió mới về phong
+    <p class="header-first-line-hp-1 co-header-css mulish-extrabold space-21">The Minato Residence</p>
+    <p class="header-second-line-hp-1 co-header-css icielbegum space-51">TINH HOA NHẬT BẢN</p>
+    <p class="header-second-line-hp-1 co-header-css icielbegum space-51">An yên giữa lòng phố Cảng</p>
+    <div class="row-test-1">
+        <div class="left-test-1" style="padding-left: 4%; padding-top: 4%">
+            <p class="first-text-hp text-content1-sec2-hp space-15 text-hp cabin-medium pb-4">Tiên phong mang đến một làn gió mới về phong
                 cách sống và đẳng cấp từ Nhật
                 Bản, The Minato
                 Residence được xây dựng bên bờ sông Lạch Tray, thừa hưởng trọn vẹn lợi thế về phong thủy bậc nhất. Chắt
                 lọc những tinh hoa từ xứ Phù Tang, The Minato Residence mang đến sự cân bằng trọn vẹn giữa nhịp sống
                 hiện đại, tiện nghi và những khoảnh khắc “sống an” chan hòa cùng thiên nhiên.</p>
-            <a href="{{route('introduce.index')}}" class="double-border first-text-hp px-4 py-2 mulish-black space-21">
+            <a href="{{route('introduce.index')}}" class="double-border first-text-hp px-3 py-2 mulish-black space-21">
                 TÌM HIỂU THÊM
             </a>
         </div>
-        <img src="{{asset('images/homepage/sec2/Mask group (3).png')}}" class="col-md-8 img-sec2-hp-1">
+        <img src="{{asset('images/homepage/sec2/Mask group (3).png')}}" class="img-sec2-hp-1 right-test-1">
     </div>
     <br>
-    <div class="row-test">
-        <img src="{{asset('images/homepage/sec2/image 6.png')}}" class="col-md-8 img-sec2-hp-2">
-        <div class="col-md-4" style="padding-right: 4%">
-            <p class="header-first-line-hp space-21 header-content2-sec2-hp mulish-extrabold">vị trí dự án</p>
+    <div class="row-test" style="padding-top: 2%">
+        <img src="{{asset('images/homepage/sec2/Bản đồ hạ tầng 1.png')}}" class="left-test img-sec2-hp-2">
+        <div class="d-block" style="">
+            <p class="header-first-line-hp space-21 header-content2-sec2-hp mulish-extrabold pb-2">vị trí dự án</p>
             <p class="header-second-line-hp header-content2-sec2-hp space-51 icielbegum">KẾ SÔNG</p>
             <p class="header-second-line-hp header-content2-sec2-hp icielbegum space-51">CẬN THỦY</p>
             <p class="header-second-line-hp header-content2-sec2-hp icielbegum space-51">ĐỊA THẾ</p>
             <p class="header-second-line-hp header-content2-sec2-hp icielbegum space-51">PHÚ QUÝ</p>
+            <div class="d-block" style="width: 20vw; padding-top: 2%; padding-bottom: 2%">
             <p class="text-hp text1-content2-sec2-hp cabin-medium space-15">The Minato Residence tọa lạc bên bờ sông
                 Lạch Tray thơ mộng, thừa
                 hưởng trọn vẹn lợi thế về
                 phong thủy hội tụ “vượng khí tài lộc” đắc địa bậc nhất thành phố Cảng Hải Phòng. Điều này mang tới nguồn
                 sinh khí dồi dào và hưng thịnh cho từng căn hộ nơi đây. </p>
-            <p class="text-hp cabin-medium pb-4 space-15">Nằm tại lõi trung tâm mới của thành phố Hải Phòng, The Minato
-                Residence là tâm điểm giao
-                thoa
-                với hệ tiện ích ngoại khu đạt chuẩn quốc tế đáp ứng mọi nhu cầu từ ăn uống, vui chơi, giải trí đến mua
-                sắm, giáo dục, y tế chỉ trong vài bước chân.</p>
+
+{{--            <p class="text-hp  text1-content2-sec2-hp cabin-medium pb-4 space-15">Nằm tại lõi trung tâm mới của thành phố Hải Phòng, The Minato--}}
+{{--                Residence là tâm điểm giao--}}
+{{--                thoa--}}
+{{--                với hệ tiện ích ngoại khu đạt chuẩn quốc tế đáp ứng mọi nhu cầu từ ăn uống, vui chơi, giải trí đến mua--}}
+{{--                sắm, giáo dục, y tế chỉ trong vài bước chân.</p>--}}
+            </div>
             <div class="btn-content2-sec2-hp">
                 <a href="{{route('location.index')}}"
-                   class="btn-content2-sec2-hp double-border px-5 py-2 mulish-black space-21">TÌM HIỂU THÊM</a>
+                   class="btn-content2-sec2-hp double-border px-4 py-2 mulish-black space-21">TÌM HIỂU THÊM</a>
             </div>
         </div>
     </div>
