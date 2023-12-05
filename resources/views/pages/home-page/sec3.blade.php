addd7a65e89747c9e5a5f26a2df0e072f07aa86e
Vinh <getback1110@gmail.com>
2023-12-05 23:40:42 +0700

    fix theo y/c 05/12

--- resources/views/pages/home-page/sec3.blade.php
+++ resources/views/pages/home-page/sec3.blade.php
@@ -2,22 +2,23 @@
     <div class="row-test">
         <div class="col-12 row-test">
             <img src="{{asset('images/homepage/sec3/17-17 1.png')}}" class="col-4">
+            <img src="{{asset('images/homepage/sec3/Group 104 (2).png')}}" class="col-9">
         </div>
-        <div class="col-12 row-test">
-            <img src="{{asset('images/homepage/sec3/Group 83.png')}}" class="col-xl-3 col-md-4 left-img-sec3-hp">
-            <img src="{{asset('images/homepage/sec3/Group 78.png')}}" class="col-xl-3 col-md-4 mid-img-sec3-hp">
-            <img src="{{asset('images/homepage/sec3/Group 77.png')}}" class="col-xl-3 col-md-4 right-img-sec3-hp">
-        </div>
-        <div class="col-12 row-test">
-            <img src="{{asset('images/homepage/sec3/Group 82.png')}}" class="col-xl-3 col-md-4 left-img-sec3-hp">
-            <img src="{{asset('images/homepage/sec3/Group 79.png')}}" class="col-xl-3 col-md-4 mid-img-sec3-hp">
-            <img src="{{asset('images/homepage/sec3/Group 76.png')}}" class="col-xl-3 col-md-4 right-img-sec3-hp">
-        </div>
-        <div class="col-12 row-test">
-            <img src="{{asset('images/homepage/sec3/Group 81.png')}}" class="col-xl-3 col-md-4 left-img-sec3-hp">
-            <img src="{{asset('images/homepage/sec3/Group 80.png')}}" class="col-xl-3 col-md-4 mid-img-sec3-hp">
-            <img src="{{asset('images/homepage/sec3/Group 75.png')}}" class="col-xl-3 col-md-4 right-img-sec3-hp">
-        </div>
+{{--        <div class="col-12 row-test">--}}
+{{--            <img src="{{asset('images/homepage/sec3/Group 83.png')}}" class="col-xl-3 col-md-4 left-img-sec3-hp">--}}
+{{--            <img src="{{asset('images/homepage/sec3/Group 78.png')}}" class="col-xl-3 col-md-4 mid-img-sec3-hp">--}}
+{{--            <img src="{{asset('images/homepage/sec3/Group 77.png')}}" class="col-xl-3 col-md-4 right-img-sec3-hp">--}}
+{{--        </div>--}}
+{{--        <div class="col-12 row-test">--}}
+{{--            <img src="{{asset('images/homepage/sec3/Group 82.png')}}" class="col-xl-3 col-md-4 left-img-sec3-hp">--}}
+{{--            <img src="{{asset('images/homepage/sec3/Group 79.png')}}" class="col-xl-3 col-md-4 mid-img-sec3-hp">--}}
+{{--            <img src="{{asset('images/homepage/sec3/Group 76.png')}}" class="col-xl-3 col-md-4 right-img-sec3-hp">--}}
+{{--        </div>--}}
+{{--        <div class="col-12 row-test">--}}
+{{--            <img src="{{asset('images/homepage/sec3/Group 81.png')}}" class="col-xl-3 col-md-4 left-img-sec3-hp">--}}
+{{--            <img src="{{asset('images/homepage/sec3/Group 80.png')}}" class="col-xl-3 col-md-4 mid-img-sec3-hp">--}}
+{{--            <img src="{{asset('images/homepage/sec3/Group 75.png')}}" class="col-xl-3 col-md-4 right-img-sec3-hp">--}}
+{{--        </div>--}}
     </div>
 </div>
 
