4d3e12465cc4352acb3ffde7b13c49ec3e087bcf
TheV1nh <115868765+TheV1nh@users.noreply.github.com>
2023-12-21 14:26:56 +0700

    Update index.blade.php

--- resources/views/pages/Tien-do/index.blade.php
+++ resources/views/pages/Tien-do/index.blade.php
@@ -1,5 +1,9 @@
 @extends('page-layout.master')
 @section('section')
     @include('pages.Introduce.sec1')
-    @include('pages.Tien-do.sec2')
+    @if(app()->getLocale() === 'vi')
+        @include('pages.Tien-do.sec2')
+    @elseif(app()->getLocale() === 'en')
+        @include('pages.Tien-do.sec2-eng')
+    @endif
 @stop
