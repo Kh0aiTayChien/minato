3f5b8778f0d4db4450800a3f3f73da3dce4509e8
chien.le <chien.hcckt@gmail.com>
2024-01-15 05:20:45 +0700

    admin-image-fix-232

--- resources/views/pages/Tien-do/sec2-eng.blade.php
+++ resources/views/pages/Tien-do/sec2-eng.blade.php
@@ -15,6 +15,7 @@
     </form>
 </div>
 <div class="secret d-none">
+{{--    test--}}
     @foreach($images_tien_do_en as $key => $image)
         <div class="carousel-item  {{$key == 0 ? 'active' : ''}}">
             <img src="{{$image->image_url}}" class="w-100 img-fluid" alt="img-slide" data-url="{{$image->url}}">
